b8113dcfb7300f1ff9a347dc25766b45759b2011
wela-herveyyy <hervey.geralph@livro.systems>
2026-04-29 19:38:18 +0800

    [feat] add wide screen

--- components/atoms/PageContainer.tsx
+++ components/atoms/PageContainer.tsx
@@ -18,10 +18,12 @@ export function PageContainer({
   };
 
   return (
-    <main
-      className={`${sizeMap[size]} h-[calc(100vh-76px)] overflow-y-auto mx-auto py-12 md:py-16 px-4 md:px-8 ${className}`}
-    >
-      {children}
-    </main>
+    <div className="h-[calc(100vh-76px)] overflow-y-auto w-screen">
+      <div
+        className={`${sizeMap[size]}  mx-auto py-12 md:py-16 px-4 md:px-8 ${className}`}
+      >
+        {children}
+      </div>
+    </div>
   );
 }
